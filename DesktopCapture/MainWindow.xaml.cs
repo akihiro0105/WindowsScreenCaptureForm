aab48954a7a5bb201d8d1bf58d232cbf83528a88
晃弘 上山 <akihiro@hololab.co.jp>
2019-05-02 18:11:42 +0900

    first commit

---
+++ DesktopCapture/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using Window = System.Windows.Window;
+
+namespace DesktopCapture
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var img = new Bitmap(1280, 720, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            var option = BitmapSizeOptions.FromWidthAndHeight(img.Size.Width, img.Size.Height);
+            var g = Graphics.FromImage(img);
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    this.Dispatcher.Invoke((Action) (() =>
+                    {
+                        g.CopyFromScreen(10, 10, 0, 0, img.Size);
+
+                        var hbitmap = img.GetHbitmap();
+                        CaptureImage.Source = Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, option);
+                        DeleteObject(hbitmap);
+                    }));
+                    await Task.Delay(50);
+                }
+            });
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+    }
+}
---
+++ DesktopCaptureWindowsForms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Point = System.Drawing.Point;
+
+namespace DesktopCaptureWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height, PixelFormat.Format24bppRgb);
+            var g = Graphics.FromImage(bmp);
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    pictureBox1.Invoke(new PictureEventHandler(() =>
+                    {
+                        var p = pictureBox1.PointToScreen(new Point(0, 0));
+                        g.CopyFromScreen(p.X, p.Y, 0, 0, bmp.Size);
+                        pictureBox1.Image = bmp;
+                    }));
+                    await Task.Delay(10);
+                }
+            });
+        }
+
+        private delegate void PictureEventHandler();
+    }
+}
